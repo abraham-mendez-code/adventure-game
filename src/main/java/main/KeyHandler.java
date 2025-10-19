178695d516b392190e6833b6beb3d8213054a4db
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-19 15:40:50 -0400

    Updated KeyHandler.keyPressed() to change the directional movement booleans based on user input. Updated KeyHandler.keyReleased to change the directional movement booleans based on user input. Together these methods update the movement direction when a key is pressed and then released.

--- src/main/java/main/KeyHandler.java
+++ src/main/java/main/KeyHandler.java
@@ -14,15 +14,49 @@ public class KeyHandler implements KeyListener {
 
     }
 
+    // this method checks for user key press and updates movement direction
     @Override
     public void keyPressed(KeyEvent e) {
 
+        // geyKeyCode returns the integer keyCode associated with the key in this event
+        int code = e.getKeyCode();
 
+        // these statements check if the user pressed one of the WASD keys and updates direction booleans based on input
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
 
     }
 
+    // this method checks for user key relead and updated movement direction
     @Override
     public void keyReleased(KeyEvent e) {
+
+        // geyKeyCode returns the integer keyCode associated with the key in this event
+        int code = e.getKeyCode();
+
+        // these statements check if the user released one of the WASD keys and updates direction booleans based on input
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
 
     }
 }
