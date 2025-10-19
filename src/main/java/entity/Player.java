bd3dec27975eedf5d4a616c3424f299c62aedd59
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-19 19:30:37 -0400

    Added movement animation files for the player.

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity{
 
@@ -16,6 +19,7 @@ public class Player extends Entity{
         this.keyHandler = keyHandler;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
@@ -23,31 +27,73 @@ public class Player extends Entity{
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
+    // this method sets the player image for each direction
+    public void getPlayerImage() {
+
+        try {
+
+            // set the image for each direction
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_up_1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_up_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_down_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_left_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_right_2.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // this methods updates player positon and direction faced based on key input
     public void update() {
 
-        // this will update player positon based on key input
+        // this will update player positon and direction based on key input
         if(keyHandler.upPressed == true) {
+            direction = "up";
             y -= speed;
         }
         else if (keyHandler.downPressed == true) {
+            direction = "down";
             y += speed;
         }
         else if (keyHandler.leftPressed == true) {
+            direction = "left";
             x -= speed;
         }
         else if (keyHandler.rightPressed == true) {
+            direction = "right";
             x += speed;
         }
 
     }
 
     public void draw(Graphics2D  g2) {
-        // this sets a color to use for drawing objects
-        g2.setColor(Color.white);
+        BufferedImage image = null;
 
-        // this draws a rectangle to the screen, this will be the player object drawn at (playerX, playerY) with a height and width of TILE_SIZE
-        g2.fillRect(x, y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE);
+        // this switch sets the image based on the direction
+        switch(direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+
+        // Draw the image based on direction
+        g2.drawImage(image, x, y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
+
     }
 }
