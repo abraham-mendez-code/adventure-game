c943e0005aa53bc3b39d80e353f380f84cf8ea79
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-19 15:36:14 -0400

    Added the KeyHandler class, a subclass of KeyListener. Added KeyHandler.keyTyped(), KeyHandler.keyPressed() and KeyHandler.keyReleased() which are required implementations. Added directional booleans to the KeyHandler class for each movement direction.

---
+++ src/main/java/main/KeyHandler.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    // declare direction booleans for each movement direction
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    // this method is not used
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
