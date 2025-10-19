c4e0f95bf521f7eeb5ad97ee6de0c08f419aaa4a
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-19 19:37:59 -0400

    Updated directory location for Player resources. Fixed but where player movement files could not be found.

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -36,14 +36,14 @@ public class Player extends Entity{
         try {
 
             // set the image for each direction
-            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_up_1.png"));
-            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_up_2.png"));
-            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_down_1.png"));
-            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_down_2.png"));
-            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_left_1.png"));
-            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_left_2.png"));
-            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_right_1.png"));
-            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("/player/player_right_2.png"));
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_up_1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_up_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_down_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_left_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/player_right_2.png"));
         }
         catch (IOException e) {
             e.printStackTrace();
