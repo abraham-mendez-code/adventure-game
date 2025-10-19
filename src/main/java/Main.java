1bf3407efdcc8ef47db471eb94f7fcf619d5c0e5
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-19 14:22:57 -0400

    Added Main class which creates a window and set its parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
+import javax.swing.*;
+
 public class Main {
 
+    private static final String TITLE = "Adventure Game";
+
     public static void main(String[] args) {
-        
+
+        // create a window using JFrame
+        JFrame window = new JFrame();
+
+        // this lets the window properly close when user clicks the close ("x") button
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // this makes the window unable to be resized
+        window.setResizable(false);
+
+        // this sets the title of the window
+        window.setTitle(TITLE);
+
+        // this makes the window location non-specific and makes the window be displayed at the center of the screen
+        window.setLocationRelativeTo(null);
+
+        // this makes the window visible
+        window.setVisible(true);
+
     }
 
 }
