1dc640fbf4f4d21e22dad40dd340080a6a0493b3
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-28 14:20:25 -0400

    Added comments to Entity class

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -5,10 +5,10 @@ import java.awt.image.BufferedImage;
 // This stores variables that will be used in player, monster and NPC classes.
 public class Entity {
 
-    public int x,y;
+    public int x,y; //position
     public int speed;
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2; // movement animation images
     public String direction;
 
     public int spriteCounter = 0;
