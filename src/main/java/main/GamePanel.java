02cb6d6dee535ccc0cdcab146373dedd07c2cd0c
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-28 15:50:52 -0400

    Added TileManager class, added tile and Map resources. Updated attribute access modifier to public for screen col, row , width, and height.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,15 +13,16 @@ public class GamePanel extends JPanel implements Runnable {
     final int SCALE = 3; // multiples the originalTileSize by this amount
 
     public final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; // scale the tile size ( (16x16) x 3 = 48x48 tile)
-    final int MAX_SCREEN_COL = 16; // this stores columns (pixels)
-    final int MAX_SCREEN_ROW = 12; // this stores rows (pixels)
+    public final int MAX_SCREEN_COL = 16; // this stores columns (pixels)
+    public final int MAX_SCREEN_ROW = 12; // this stores rows (pixels)
 
-    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // (48 x 16 = 768 pixels) multiply the columns by the scaled tile size to get the width
-    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // (48 x 12 = 576 pixels) multiply the rows by the scaled tile size to get the height
+    public final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // (48 x 16 = 768 pixels) multiply the columns by the scaled tile size to get the width
+    public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // (48 x 12 = 576 pixels) multiply the rows by the scaled tile size to get the height
 
     // FPS
     int FPS = 60;
 
+    TileManager tileManager = new TileManager(this);
     KeyHandler keyHandler = new KeyHandler(); // KeyHandler allows receipt of user keyboard input
     Thread gameThread; // Thread allows for control over starting and stopping of a program, can be used for game FPS
     Player player = new Player(this, keyHandler); // Instantiate a player class with this game panel and the keyHandler
@@ -141,6 +143,8 @@ public class GamePanel extends JPanel implements Runnable {
         // declare a 2D graphics object and, assign it Graphics cast as Graphics2D
         Graphics2D g2 =  (Graphics2D) g;
 
+        // draw in layers, last draw is at the top, first draw is at the bottom
+        tileManager.draw(g2);
         player.draw(g2);
 
         g2.dispose(); // dispose of this graphics context and release any system resources that it is using.
---
+++ src/main/java/tile/TileManager.java
@@ -0,0 +1,117 @@
+package tile;
+
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TileManager {
+
+    // class attributes
+    GamePanel gp;
+    Tile[] tile;
+    int mapTileNumber[] []; // this array holds the values for map tiles
+
+    public TileManager(GamePanel gp) {
+
+        this.gp = gp;
+
+        tile = new Tile[10]; // set size of tile array, equal to the amount of different tiles
+        mapTileNumber = new int[gp.MAX_SCREEN_COL][gp.MAX_SCREEN_ROW]; // max col by max row
+
+        getTileImage();
+        loadMap("maps/maps01.txt");
+    }
+
+    // this method loads the tile images
+    public void getTileImage() {
+
+        try {
+
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/grass00.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall.png"));
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/water00.png"));
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMap(String filePath) {
+
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath); // input stream imports a text file
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); // buffered reader allows reading of text file
+
+            int col = 0;
+            int row = 0;
+
+            // this loops for every col and row on the screen
+            while (col < gp.MAX_SCREEN_COL && row < gp.MAX_SCREEN_ROW) {
+
+                String line = bufferedReader.readLine(); // read a line from the file
+
+                while (col < gp.MAX_SCREEN_COL) { // for every col in current row
+
+                    String[] numbers = line.split(" "); // split the current line into its numbers
+
+                    int number = Integer.parseInt(numbers[col]); // get the number in the current col
+
+                    mapTileNumber[col] [row] = number; // assign the tile number to the current col and row
+                    col++; // move to the next col
+                }
+
+                if (col == gp.MAX_SCREEN_COL) { // if max col is reached
+                    col = 0; // reset it
+                    row++; // go to next row
+                }
+
+            }
+            bufferedReader.close();
+        }
+        catch (Exception e) {
+
+        }
+
+
+    }
+
+    // draws tiles to the GamePanel
+    public void draw(Graphics2D g2) {
+
+        // declare and initialize draw variables
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        // this loop draws
+        while (col < gp.MAX_SCREEN_COL && row < gp.MAX_SCREEN_ROW) {
+
+            int tileNumber = mapTileNumber[col] [row];
+
+            g2.drawImage(tile[tileNumber].image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
+            col++;
+            x += gp.TILE_SIZE;
+
+            if (col == gp.MAX_SCREEN_COL) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.TILE_SIZE;
+            }
+        }
+
+    }
+
+}
